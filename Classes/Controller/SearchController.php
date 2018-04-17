fcae5f7ecbad434737a5112e989d38174f10e87e
Christopher Timm <timm@effective-webwork.de>
2018-04-17 07:09:09 +0200

    redirect to list if the q parameter is missing in the url

--- Classes/Controller/SearchController.php
+++ Classes/Controller/SearchController.php
@@ -93,6 +93,11 @@ class SearchController extends ActionController
      */
     public function indexAction()
     {
+        if(!array_key_exists('start', $this->requestArguments)) {
+            $params = array('start' => '1');
+            $this->redirect('index', NULL, NULL, array_merge($this->requestArguments, $params));
+        }
+
         if (array_key_exists('id', $this->requestArguments)) {
             $this->forward('detail');
         } else {
