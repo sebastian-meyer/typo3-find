470a2710400ce4ab4aea5c127971f97854dba19c
Christopher Timm <timm@effective-webwork.de>
2024-04-02 13:38:32 +0200

    Changes v12

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,18 +3,18 @@
 $EM_CONF['find'] = [
     'title' => 'Find',
     'description' => 'A frontend for Solr indexes',
-    'version' => '3.1.1',
+    'version' => '4.0.2',
     'state' => 'stable',
-    'category' => 'frontend',
+    'category' => 'plugin',
     'clearCacheOnLoad' => true,
     'author' => 'Sven-S. Porst, Ingo Pfennigstorf',
     'author_email' => 'pfennigstorf@sub.uni-goettingen.de',
     'author_company' => 'SUB Göttingen',
     'constraints' => [
         'depends' => [
-            'php' => '7.4.0-8.0.99',
-            'typo3' => '10.4.0-10.4.99',
-            'felogin' => '10.4.0-10.4.99',
+            'php' => '7.4.0-8.1.99',
+            'typo3' => '10.4.0-12.4.99',
+            'felogin' => '10.4.0-12.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
